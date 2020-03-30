761c44963901c96fea8b54a6eac070d2767b428c
proloving <2667737165@qq.com>
2020-03-30 17:07:03 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,67 @@
+//index.js
+const app = getApp()
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrl: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  selectImg: function() {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sourceType: ['album', 'camera'],
+      sizeType: ['original', 'compressed'],
+      success: function(res) {
+        console.log('select image success')
+        console.log(res)
+        that.setData({
+          imgUrl: res.tempFilePaths[0],
+        });
+      },
+      fail: function() {
+        console.log('select image failed');
+      }
+    })
+  },
+
+  uploadImg: function() {
+    var that =this
+    wx.showLoading({
+      title: '上传中',
+    })
+
+    wx.uploadFile({
+      url: 'https://proloving.imwork.net/uploadImg', //仅为示例
+      filePath: that.data.imgUrl,
+      name: 'parkingPhoto',
+      success: function (res) {
+        console.log(res)
+        wx.showToast({
+          icon: 'none',
+          title: '上传成功',
+        })
+      },
+      fail:function(res){
+        console.log(res)
+        wx.showToast({
+          icon: 'none',
+          title: '上传失败',
+        })
+      }
+    })
+  }
+
+
+
+})
